38d14b55f92d1d19edf123a463567ee6acad588f
Ayla Pearson <ayla@poissonconsulting.ca>
2023-04-08 16:26:06 -0700

    clean up

--- R/raw-data.R
+++ R/raw-data.R
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 #' Sample of Raw Data
 #' 
 #' Sample of raw set used for testing. 
